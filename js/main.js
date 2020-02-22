482920592de20e4d1f038bd82096cadd72cb8dd6
Elmer <s0227373691@gms.tku.edu.tw>
2020-02-22 13:08:19 +0800

    revision meta tag

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+$(function(){
+    const rule = /[0-9][^a-z]/;
+    $('#test_input').blur(function(){
+        const test_input = $('#test_input').val();
+        if(rule.test(test_input)){
+            alert('true');
+        }else{
+            alert('false')
+        }
+    })
+    
+})
 
-let main = {
-    click: function () {
-    }
-}
-
-$(main.click());
 
